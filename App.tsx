aedb3fec0d49d89576daf7cdf06de8ff9569748a
techvfxking <jobs.biplab.sharma@gmail.com>
2024-12-01 00:17:39 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const App = (): React.JSX.Element => {
+ return(
+  <>
+    <View>
+      
+    </View>
+  </>
+ )
+}
+
+export default App;
